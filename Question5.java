b82dc6f705eb0c4d725bef1c3efdcd8cdc15f67e
Kuah Hsu Yuan <93971345+hsuyuan123@users.noreply.github.com>
2022-08-17 10:07:39 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -4,42 +4,35 @@ public class Question5
 {
   public static void main(String[] args)
   {
-    /**
-     * Prompt the user for number. This input indicates the number of integers the user will be entering next. 
-     * Print out the mode (highest occurrence) from the set of integers. 
-     
-     *    e.g.
-     *     > 5
-     *     > 2
-     *     > 4
-     *     > 1
-     *     > 3
-     *     > 4
-     *     4
-     * 
-     *    e.g.
-     *     > 4
-     *     > 2
-     *     > 2
-     *     > 3
-     *     > 3
-     *     2
-     * Hint: Use a loop to get input. Use another 2 loops to find the mode
-     */
-     
     Scanner in = new Scanner(System.in);
-    System.out.println("Enter integer: ");
-    int number = in.nextInt();
-    const numberlist = [];
-    int index = 1;
-    while (index >= number)
+    int i = 0;
+    System.out.print("Enter number of integer to be keyed in: ");
+    int c = in.nextInt();
+    int[] myArray = new int[c];
+    for(i = 0; i < c; i++)
     {
-      System.out.println("Enter integer: ");
-      int newnumber = in.nextInt();
-      numberlist.push(newnumber);
-      index++;
+      System.out.print("Enter a value: ");
+      myArray[i] = in.nextInt();
+    } 
+    int maxNumber = -1;
+    int maxAppearance = -1;
+    for(i = 0; i < myArray.length; i++)
+    {
+      int count = 0;
+      for (int j = 0; j < myArray.length; j++)
+      {
+        if (myArray[i] == myArray[j])
+        {
+          count++;
+        }
+
+        if (count > maxAppearance)
+        {
+          maxNumber = myArray[i];
+          maxAppearance = count;
+        }
+      }
     }
-    for(let i = 0;i < numberlist.length;i++){
-   if numberlist[i] > highest);
+    System.out.println("The mode is: " + maxNumber);
   }
 }
